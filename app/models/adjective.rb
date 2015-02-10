3883fb87c61098cd1f7c81ea7783c23581cae209
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-02-10 03:22:20 +0300

    fix #14, magic for adjectives

--- app/models/adjective.rb
+++ app/models/adjective.rb
@@ -4,6 +4,7 @@ class Adjective < ActiveRecord::Base
   validates_presence_of :nominative_masculine, :nominative_feminine, :nominative_neuter, :nominative_plural
   validates_presence_of :genitive_masculine, :genitive_feminine, :genitive_neuter, :genitive_plural
   validates_presence_of :dative_masculine, :dative_feminine, :dative_neuter, :dative_plural
+  validates_presence_of :accusative_masculine, :accusative_feminine, :accusative_neuter, :accusative_plural
   validates_presence_of :instrumental_masculine, :instrumental_feminine, :instrumental_neuter, :instrumental_plural
   validates_presence_of :prepositional_masculine, :prepositional_feminine, :prepositional_neuter, :prepositional_plural
 

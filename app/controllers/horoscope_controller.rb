b1258c13d362a189e1c4b2116c4ccbc23b8a9aff
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-03-03 05:00:28 +0300

    see #26, horoscope for signs

--- app/controllers/horoscope_controller.rb
+++ app/controllers/horoscope_controller.rb
@@ -1,7 +1,13 @@
 class HoroscopeController < ApplicationController
+  # get /horoscope
   def index
   end
 
+  # get /horoscope/:sign
   def view
+    seed   = Time.new.strftime('%Y%m%d').to_i + params[:sign].to_i(26)
+    speech = Speech.new(seed)
+
+    @forecast = speech.passage
   end
 end

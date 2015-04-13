ca86df030caaa93bcecb3b257d4a2afefc003310
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-04-13 03:15:20 +0300

    see #30, refactoring in Noun model

--- app/models/noun.rb
+++ app/models/noun.rb
@@ -2,7 +2,7 @@ class Noun < ActiveRecord::Base
   belongs_to :user
 
   validates_uniqueness_of :nominative
-  validates_presence_of :nominative, :genitive, :dative, :accusative, :instrumental, :prepositional
+  validates_presence_of :nominative, :genitive, :dative, :partitive, :accusative, :instrumental, :prepositional, :locative
 
   enum grammatical_gender: [:masculine, :feminine, :neuter]
   enum grammatical_number: [:both, :singular_only, :plural_only]
@@ -20,70 +20,13 @@ class Noun < ActiveRecord::Base
   end
 
   def decline(grammatical_case, number = :single)
-    case grammatical_case
-      when :genitive
-        in_genitive number
-      when :dative
-        in_dative number
-      when :accusative
-        in_accusative number
-      when :instrumental
-        in_instrumental number
-      when :prepositional
-        in_prepositional number
-      when :locative
-        locative number
-      else
-        in_nominative number
-    end
-  end
-
-  def in_nominative(number)
-    if number === :single
-      nominative
+    if (number === :single) || self.singular_only? || self.plural_only?
+      message = grammatical_case.to_s
     else
-      (self.plural_only? || self.singular_only?) ? nominative : plural_nominative
+      message = "plural_#{grammatical_case}"
     end
-  end
 
-  def in_genitive(number)
-    if number === :single
-      genitive
-    else
-      (self.plural_only? || self.singular_only?) ? genitive : plural_genitive
-    end
-  end
-
-  def in_dative(number)
-    if number === :single
-      dative
-    else
-      (self.plural_only? || self.singular_only?) ? dative : plural_dative
-    end
-  end
-
-  def in_accusative(number)
-    if number === :single
-      accusative
-    else
-      (self.plural_only? || self.singular_only?) ? accusative : plural_accusative
-    end
-  end
-
-  def in_instrumental(number)
-    if number === :single
-      instrumental
-    else
-      (self.plural_only? || self.singular_only?) ? instrumental : plural_instrumental
-    end
-  end
-
-  def in_prepositional(number)
-    if number === :single
-      prepositional
-    else
-      (self.plural_only? || self.singular_only?) ? prepositional : plural_prepositional
-    end
+    respond_to?(message) ? send(message) : "[#{canonical_form}/#{grammatical_case}/#{number}]"
   end
 
   def number

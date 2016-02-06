71d737733d94ce5d7ecc94ae8d4d71f705cfa84b
Maxim Khan-Magomedov <maxim.km@gmail.com>
2016-02-06 20:19:45 +0300

    Spec for Word

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Word, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'passes with valid attributes' do
+      word = build :word
+      expect(word).to be_valid
+    end
+
+    it 'fails with empty body' do
+      word = build :word, body: ' '
+      expect(word).not_to be_valid
+    end
+
+    it 'fails with non-unique body for same stress' do
+      existing = create :word, stress: '10'
+      word = build :word, body: existing.body, stress: existing.stress
+      expect(word).not_to be_valid
+    end
+  end
 end

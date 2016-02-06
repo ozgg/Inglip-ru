5c8fc4e117d28bde9c4a88e3ab1d8ceb39b6afac
Maxim Khan-Magomedov <maxim.km@gmail.com>
2016-02-06 20:13:37 +0300

    Spec for lexeme

--- spec/models/lexeme_spec.rb
+++ spec/models/lexeme_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Lexeme, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'passes with valid attributes' do
+      lexeme = build :lexeme
+      expect(lexeme).to be_valid
+    end
+
+    it 'fails without body' do
+      lexeme = build :lexeme, body: ' '
+      expect(lexeme).not_to be_valid
+    end
+
+    it 'fails with non-unique body in context and part' do
+      existing = create :lexeme
+      lexeme   = build :lexeme, body: existing.body, context: existing.context, part: existing.part
+      expect(lexeme).not_to be_valid
+    end
+  end
 end

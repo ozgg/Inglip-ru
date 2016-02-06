82fb5528355fe2e4789a9860bc882a9843f4d3b2
Maxim Khan-Magomedov <maxim.km@gmail.com>
2016-02-06 20:36:51 +0300

    Spec for user

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -2,6 +2,7 @@ class UserRole < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :user_id, :role
+  validates_uniqueness_of :role, scope: [:user_id]
 
   enum role: [:administrator]
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'passes with valid attributes' do
+      user = build :user
+      expect(user).to be_valid
+    end
+
+    it 'fails without login' do
+      user = build :user, login: ' '
+      expect(user).not_to be_valid
+    end
+
+    it 'fails with non-unique login' do
+      existing = create :user
+      user     = build :user, login: existing.login
+      expect(user).not_to be_valid
+    end
+  end
 end

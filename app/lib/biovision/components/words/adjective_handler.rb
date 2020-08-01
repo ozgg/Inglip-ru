3bdf55655f24fa299fd87d0ea522325d37d7ca38
Maxim Khan-Magomedov <maxim.km@gmail.com>
2020-08-02 02:32:54 +0300

    Added forms for adjectives

--- app/lib/biovision/components/words/adjective_handler.rb
+++ app/lib/biovision/components/words/adjective_handler.rb
@@ -6,18 +6,22 @@ module Biovision
       # Adjective handler
       class AdjectiveHandler < LexemeHandler
         def self.lexeme_data_flags
-          super + %w[qualitative]
+          %w[qualitative superlative]
         end
 
         def self.wordform_flags
           sets = [
             gender_flags, number_flags, case_flags, adjective_form_flags,
-            comparative_form_flags, superlative_form_flags
+            comparative_form_flags
           ]
           result = super
           sets.each { |part| result.merge! part }
 
           result
+        end
+
+        def qualitative?
+          lexeme.flag?(:qualitative)
         end
       end
     end

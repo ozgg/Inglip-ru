90aaa36af36668340ab3b4192a5c63a85e6ae630
Maxim Khan-Magomedov <maxim.km@gmail.com>
2020-08-07 23:50:33 +0300

    Editing prepositions

--- app/lib/biovision/components/words/preposition_handler.rb
+++ app/lib/biovision/components/words/preposition_handler.rb
@@ -6,7 +6,7 @@ module Biovision
       # Preposition handler
       class PrepositionHandler < LexemeHandler
         def self.lexeme_data_flags
-          super + lexeme_valency_flags + %w[vowel_ending consonant_ending]
+          lexeme_valency_flags + %w[vowel_ending consonant_ending]
         end
 
         def self.wordform_flags

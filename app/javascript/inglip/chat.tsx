74dfe5f216d06b3d0557c5ac2c046faf33d3b987
Maxim Khan-Magomedov <maxim.km@gmail.com>
2022-01-13 02:00:00 +0300

    Adding user messages to chat

--- app/javascript/inglip/chat.tsx
+++ app/javascript/inglip/chat.tsx
@@ -1,9 +1,33 @@
-import * as React from "react";
+import React, {useState} from "react";
 
 export default function Chat() {
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        setMessages(prevMessages => prevMessages.concat([message]))
+        setMessage('')
+    }
+
+    function handleChange(event) {
+        setMessage(event.target.value)
+    }
+
     return (
         <section>
             <h1>Chat</h1>
+            <section className="chat-messages">
+                <ul>{messages.map((m, i) => <li key={i}>{m}</li>)}</ul>
+            </section>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <textarea value={message} onChange={handleChange}/>
+                </div>
+                <div>
+                    <button>Отправить</button>
+                </div>
+            </form>
         </section>
     )
 }
